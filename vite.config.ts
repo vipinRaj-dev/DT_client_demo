58318e8dbe5e5a3e1741c0b479c8f4e3e1bc6c7b
vipin <vipinrj8473@gmail.com>
2025-01-13 12:34:43 +0530

    Configured GitHub Pages deployment for Vite app

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/DT_client_demo",
+  base: "/DT_client_demo/",
 });
